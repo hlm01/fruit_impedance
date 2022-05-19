9ef3509c0f3a413bbc9b770e52718db541e08b78
magikarp620 <magikarp620@github.com>
2022-05-19 16:04:27 -0700

    Update algorithm file

---
+++ algorithm.py
@@ -0,0 +1,7 @@
+import csv
+with open('data/Impedance & Phase Data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    # row[0] is the frequency row[1] is the Impedance
+    fruit_dict = {int(rows[0]):float(rows[1]) for rows in reader}
+    print(fruit_dict)
