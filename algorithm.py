9c504e9b77a7c6a37a366f28ebb07654715a70bb
hlm01 <35647316+hlm01@users.noreply.github.com>
2022-05-22 18:11:03 -0700

    add graph

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,11 @@
 import csv
 
 def make_dictionary():
-    with open('data/Impedance & Phase Data.csv', newline='') as csvfile:
+    with open('data/Impedance & Phase Data.csv', "r+",newline='') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         # row[0] is the frequency row[1] is the Impedance
         fruit_dict = {int(rows[0]):float(rows[1]) for rows in reader}
+        csvfile.seek(0)
+        csvfile.truncate() # delete contents of file
         return fruit_dict
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from json import tool
 import tkinter as tk
 from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import get_data
 import algorithm
+from time import sleep
 
 
 class App(tk.Tk):
@@ -10,7 +14,7 @@ class App(tk.Tk):
 
         # root window
         self.title('Fruit Ripeness:')
-        self.geometry('500x200')
+        self.geometry('700x700')
         self.resizable(False,False)
         self.style = ttk.Style(self)
 
@@ -25,6 +29,7 @@ class App(tk.Tk):
         #current_value = tk.DoubleVar()
         current_value = 5
         
+        self.setup_plot()
         def get_current_value():
             #return '{: .2f}'.format(current_value.get())
             return current_value
@@ -57,7 +62,9 @@ class App(tk.Tk):
         slider.grid(
             column=1,
             row=0,
-            sticky='we'
+            sticky='we',
+            pady = 25,
+            padx = 10
         )
 
         # current value label
@@ -85,12 +92,33 @@ class App(tk.Tk):
             sticky='n'
         )
 
+    # plot the data with matplotlib
+    def setup_plot(self):
+        # draw figure
+        fig = Figure(figsize = (10,7), dpi=50)
+        self.canvas = FigureCanvasTkAgg(fig, master = self)
+        self.plot1 = fig.add_subplot()
+
+       
+    def plot_data(self):
+        self.plot1.cla()
+        freq = list(self.measurement.keys())
+        values = list(self.measurement.values())
+
+        vmax, vmin = max(values), min(values)
+        self.plot1.plot(freq,values) # plot values
+        self.plot1.set_xlabel("frequency hz")
+        self.plot1.set_ylabel("impedance ohms")
+        self.plot1.set_ylim(vmin*0.9,vmax*1.1)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(column = 1,padx = 30, sticky="N")
     # runs script to collect data
     def button_clicked(self):
         print("clicked")
         get_data.get_data()
+        sleep(2)
         self.measurement = algorithm.make_dictionary()
-        print(self.measurement)
+        self.plot_data()
 
 
 if __name__ == "__main__":
